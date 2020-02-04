877114c627f013632ef89512ceafabb28016c2c4
root <root@localhost.localdomain>
2020-02-04 07:54:13 -0500

    adding my calcluater

--- calculator.py
+++ calculator.py
@@ -1,8 +1,31 @@
-
 def main():
 	#write your code here
-	pass
-	
+        number1 =input("Enter the first number: ")
+        number2 =input("Enter the second number: ")
+        operation = input("Choose the operation (+, -, /, *): ")
+        if number1.isalpha():
+            print("The number were invalid")
+        else:
+            if number2.isalpha():
+                print("The number were inalid")
+            else:
+                if operation == "+":
+                    x=int(number1)+int(number2)
+                    print("The answer is "+str(x))
+                elif operation == "-":
+                    y = int(number1)-int(number2)
+                    print("The answer is "+str(y))
+                elif operation == "/":
+                    z =int(number1)/int(number2)
+                    print("The answer is "+str(z))
+                elif operation == "*":
+                    v=int(number1)*int(number2)
+                    print("The answer is "+str(v))
+                else:
+                    print("The operation is not valid ")
+
+
+        
 
 
 
